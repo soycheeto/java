e26010216d1c1c80ec34c19b17531fe5bea88646
chito <trishahansen.ihs@gmail.com>
2025-01-23 10:36:29 +0530

    Update Constru.java

--- Constru.java
+++ Constru.java
@@ -1,8 +1,15 @@
 class Constru{
-    Constru(){
-        System.out.println("HIIIII :D");
+    Constru(int a){
+        System.out.println("HIIII :D");
+        System.out.println(a);
+    }
+    Constru(int a, int b){
+        System.out.println("Constructor Overloading");
+        System.out.println("The value of A:"+a);
+        System.out.println("The value of B:"+b);                       
     }
     public static void main(String args[]){
-        Constru c = new Constru();
+        Constru c = new Constru(10);
+        Constru d = new Constru(10,20);
     }
 }
